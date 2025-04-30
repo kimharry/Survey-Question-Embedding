1e7a474646fad517884a7ed8c157df55ea0d9cdc
kimharry <kimharry0610@gmail.com>
2025-05-01 05:06:44 +0900

    tlqkf 살려줘

--- data-dropna.py
+++ data-dropna.py
@@ -9,9 +9,14 @@ data = data.drop(data.columns[0], axis=1)
 # drop columns related to stackoverflow
 data = data.drop(columns=['NEWSOSites', 'SOVisitFreq', 'SOAccount', 'SOPartFreq', 'SOHow', 'SOComm'])
 
-# drop the column if half of the answers are N/A
+# drop the rows if half of the answers are N/A
+for i, row in data.iterrows():
+    if row.isna().sum() > len(row) / 20:
+        data = data.drop(i, axis=0)
+
+# drop the columns if half of the answers are N/A
 for column in data.columns:
     if data[column].isna().sum() > len(data) / 2:
         data = data.drop(column, axis=1)
 
-data.to_csv('data/na-removed-data.csv', index=False)
+data.to_csv('data/na-removed-data2.csv', index=False)
---
+++ data-sent-trans.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+
+# Load the CSV file
+data = pd.read_csv('data/na-removed-data2.csv')
+
+vectorized = pd.DataFrame()
+
+# Load the model
+model = SentenceTransformer('all-MiniLM-L12-v2')
+
+# Get the embeddings per column
+for column in data.columns:
+    vec = model.encode(data[column].apply(lambda x: str(x) if isinstance(x, float) else x).to_list())
+    print(f"Embedding shape for {column}: {vec.shape}")
+    
+    # Convert each vector to a list and add it as a new column
+    vectorized[column] = vec.tolist()
+
+vectorized.to_csv('data/vectorized-data4.csv', index=False)
